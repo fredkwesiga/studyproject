b3f3e077196d6ea95cbb444f11c4090272ec03c1
fredkwesiga <fredkwesiga9@gmail.com>
2024-06-18 22:14:26 +0300

    works on  User Profile Page and static files

--- base/views.py
+++ base/views.py
@@ -105,6 +105,16 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_message = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_message': room_message, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
+
+
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -113,7 +123,9 @@ def createRoom(request):
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect("home")
     
     context = {'form': form, 'RoomForm': RoomForm}
